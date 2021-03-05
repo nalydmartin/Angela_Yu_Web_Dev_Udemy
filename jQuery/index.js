bdbebd68d6b3bd03b3b3fca920240a1851067e60
Dylan Martin <69947551+nalydmartin@users.noreply.github.com>
2021-03-05 15:09:48 -0800

    Made updates

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,25 @@
 
 
-$("h1.one").css("color", "red");
-$("h1.two").css("color", "blue");
+// $("h1").addEventListener("click", (() => {
+//     $("h1").addClass("extra");
+// }))
+
+
+$("h1").click(function () { 
+    console.log(this);
+    if($(this).hasClass("extra")){
+        $(this).removeClass("extra");
+    } else {
+        $(this).addClass("extra");
+    }
+});
+
+$(document).keydown(function (e) { 
+    console.log(e.key);
+    $("h1.two").text(e.key);
+});
+
+$("h1").mouseover(function () { 
+
+    $(this).css("color", "purple");
+});
